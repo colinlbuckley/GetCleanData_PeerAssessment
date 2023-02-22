d2c402283bfd79c945518281163eb4a9394ba101
Colin Buckley <colinlbuckley@gmail.com>
2023-02-21 19:37:22 -0500

    Commit with updated documentation and complete analysis script.

--- run_analysis.R
+++ run_analysis.R
@@ -32,6 +32,7 @@ rm(subject_test, activity_test, feature_test) # rm file vectors
 ## 1. Merge train and test sets ----
 merged_data <- rbind(train, test)
 names(merged_data)[1:3] <- c("subject", "activity", "V1")
+rm(train, test)
 
 # Cleaning Data ----
 ## Clean names/labels ----
@@ -42,8 +43,7 @@ feature_names_clean <- gsub("-|\\(|\\)", "", feature_names$V2) %>%
     gsub("^anglet", "angletime", .) %>%
     gsub("^anglef", "anglefreq", .) %>%
     gsub("^f", "freq", .)
-#feature_names_clean <- gsub(",", "_", feature_names_trimmed)
-rm(feature_names, feature_names_trimmed) # rm file vectors
+rm(feature_names) # rm file vector
 
 ## 4. Descriptive variable names ----
 names(merged_data)[3:563] <- feature_names_clean
@@ -52,15 +52,23 @@ names(merged_data)[3:563] <- feature_names_clean
 # note regex phrasing to exclude instances of "meanFreq()"
 mean_std <- merged_data %>%
     select(matches("subject|activity|mean$|mean[x-zX-Z]|std"))
+# glimpse(mean_std)
 
 ## 3. Set clear/descriptive labels for activity factor ----
 mean_std$activity <- factor(mean_std$activity, 
                             levels = c(1:6),
                             labels = activity_names)
 
-mean_std %>%
-    group_by(activity, subject) %>%
-    summarize(across())
-# Preview Datasets ----
-glimpse(mean_std)
-#glimpse(mean_std)
+## 5. Create separate tidy dataset with averages for each subject/activity ----
+# I'm unclear as to whether "average of each variable" in the context of this question
+# means "mean of each measurement" or "average of each column." The code below yields
+# the latter, but uncommenting the select() call will exclude standard deviation cols.
+grouped_avg <- mean_std %>%
+    #select(!matches("std")) %>%
+    group_by(subject, activity) %>%
+    summarize(across(everything(), mean))
+# glimpse(grouped_avg)
+
+# Export ----
+write.table(grouped_avg, "data/tidydata.txt", row.names = FALSE)
+rm(grouped_avg, mean_std, merged_data, activity_names, feature_names_clean)
